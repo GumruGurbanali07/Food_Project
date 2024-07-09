2121a541e332ce7494a11e0b512bf721e966b99a
GumruGurbanali07 <qurbaneliyevaqumru@gmail.com>
2024-07-09 19:19:31 +0400

    commits

---
+++ Food_Project/Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Food_Project.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Food_Project.Data
+{
+	public class AppDbContext:DbContext
+	{
+		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { 
+
+		}
+		public DbSet<Category> Categories { get; set; }
+		public DbSet<Food> Foods { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Category>()
+			  .HasKey(x => x.CategoryId);
+			modelBuilder.Entity<Food>()
+				.HasOne(y => y.Category)
+				.WithMany(x => x.Foods)
+				.HasForeignKey(y => y.CategoryId);
+		}
+	}
+}
---
+++ Food_Project/Models/Food.cs
@@ -0,0 +1,17 @@
+﻿namespace Food_Project.Models
+{
+	public class Food
+	{
+        public int FoodId { get; set; }
+        public string Name { get; set; }
+        public string LongDescription { get; set; }
+        public string ShortDescription { get; set; }
+        public double Price { get; set; }
+        public string ImageUrl { get; set; }
+        public string ThumbNailImageUrl { get; set; }
+        public int Stock { get; set; }
+
+        public int CategoryId {  get; set; }
+        public Category Category { get; set; }
+    }
+}
