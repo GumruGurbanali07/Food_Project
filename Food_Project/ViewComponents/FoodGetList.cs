9558aa8c06562a29cfb82fa632048e57b332d7f5
GumruGurbanali07 <qurbaneliyevaqumru@gmail.com>
2024-07-19 14:36:38 +0400

    commits

---
+++ Food_Project/ViewComponents/FoodGetList.cs
@@ -0,0 +1,21 @@
+﻿using Food_Project.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Food_Project.ViewComponents
+{
+	public class FoodGetList : ViewComponent
+	{
+		private readonly FoodRepository _foodRepository;
+
+		public FoodGetList(FoodRepository foodRepository)
+		{
+			_foodRepository = foodRepository;
+		}
+
+		public IViewComponentResult Invoke()
+		{
+			var res = _foodRepository.ListT();
+			return View(res);
+		}
+	}
+}
--- Food_Project/obj/Debug/net8.0/Food_Project.AssemblyInfo.cs
+++ Food_Project/obj/Debug/net8.0/Food_Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Food_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7d321340d7a5e4cba43b4a33ead088110ec1909")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aeea92a66310d475960f942b2ed78f39e59cd354")]
 [assembly: System.Reflection.AssemblyProductAttribute("Food_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Food_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
